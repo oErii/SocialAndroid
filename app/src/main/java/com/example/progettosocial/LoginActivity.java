7b69af2e7bf351e17bb13f7f58859344ccdc80ff
enric <enrico.tarulli@gmail.com>
2025-10-10 13:53:17 +0200

    login

--- app/src/main/java/com/example/progettosocial/LoginActivity.java
+++ app/src/main/java/com/example/progettosocial/LoginActivity.java
@@ -2,7 +2,6 @@ package com.example.progettosocial;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -11,20 +10,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.progettosocial.databinding.ActivityMainBinding;
+import com.example.progettosocial.databinding.ActivityLoginBinding;
 import com.example.progettosocial.model.Utente;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    private ActivityLoginBinding binding;
 
-    private  static final String USERNAME = "admin";
-    private  static final String PASSWORD = "psw";
+    private  static final String EMAIL = "enrico.tarulli@gmail.com";
+    private  static final String PASSWORD = "Psw123!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding  = ActivityMainBinding.inflate(getLayoutInflater());
+        binding  = ActivityLoginBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
         setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
@@ -35,10 +34,10 @@ public class MainActivity extends AppCompatActivity {
 
         binding.Accedi.setOnClickListener(
                 v->{
-                    if (USERNAME.equals(binding.Email.getText().toString())&& PASSWORD.equals(binding.Psw.getText().toString())){
-                        Intent intent = new Intent(MainActivity.this, PostActivity.class);
+                    if (EMAIL.equals(binding.Email.getText().toString())&& PASSWORD.equals(binding.Psw.getText().toString())){
+                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                         // intent.putExtra("username",binding.username.getText().toString());
-                        Utente utente = new Utente(USERNAME,PASSWORD,"Marco","Rossi");
+                        Utente utente = new Utente("oErii", EMAIL, PASSWORD, "Enrico", "Tarulli");
                         intent.putExtra("utente",utente);
                         startActivity(intent);
                         finish();
@@ -46,7 +45,15 @@ public class MainActivity extends AppCompatActivity {
                     else{
                         Toast.makeText(this, "Username o password errati", Toast.LENGTH_SHORT).show();
                     }
-                }d
+                }
+        );
+
+        binding.Registrati.setOnClickListener(
+                v->{
+                    Intent intent = new Intent(LoginActivity.this, RegistrazioneActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
         );
     }
 }
--- app/src/main/java/com/example/progettosocial/model/Utente.java
+++ app/src/main/java/com/example/progettosocial/model/Utente.java
@@ -4,11 +4,13 @@ import java.io.Serializable;
 
 public class Utente implements Serializable {
     private String username;
+    private String email;
     private String password;
     private String nome, cognome;
 
-    public Utente(String username, String password, String nome, String cognome) {
+    public Utente(String username, String email, String password, String nome, String cognome) {
         this.username = username;
+        this.email=email;
         this.password = password;
         this.nome = nome;
         this.cognome = cognome;
@@ -20,6 +22,9 @@ public class Utente implements Serializable {
     public void setUsername(String username) {
         this.username = username;
     }
+
+    public String getEmail() {return email;}
+    public void setEmail(String email) {this.email = email;}
     public String getPassword() {
         return password;
     }
