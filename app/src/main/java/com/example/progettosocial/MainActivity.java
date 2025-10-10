bc93af8a06a26456ca47fe6746227cd8e3e42227
enric <enrico.tarulli@gmail.com>
2025-10-10 13:29:22 +0200

    primo commit

---
+++ app/src/main/java/com/example/progettosocial/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.progettosocial;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.progettosocial.databinding.ActivityMainBinding;
+import com.example.progettosocial.model.Utente;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private  static final String USERNAME = "admin";
+    private  static final String PASSWORD = "psw";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding  = ActivityMainBinding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        binding.Accedi.setOnClickListener(
+                v->{
+                    if (USERNAME.equals(binding.Email.getText().toString())&& PASSWORD.equals(binding.Psw.getText().toString())){
+                        Intent intent = new Intent(MainActivity.this, PostActivity.class);
+                        // intent.putExtra("username",binding.username.getText().toString());
+                        Utente utente = new Utente(USERNAME,PASSWORD,"Marco","Rossi");
+                        intent.putExtra("utente",utente);
+                        startActivity(intent);
+                        finish();
+                    }
+                    else{
+                        Toast.makeText(this, "Username o password errati", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+    }
+}
---
+++ app/src/main/java/com/example/progettosocial/model/Utente.java
@@ -0,0 +1,41 @@
+package com.example.progettosocial.model;
+
+import java.io.Serializable;
+
+public class Utente implements Serializable {
+    private String username;
+    private String password;
+    private String nome, cognome;
+
+    public Utente(String username, String password, String nome, String cognome) {
+        this.username = username;
+        this.password = password;
+        this.nome = nome;
+        this.cognome = cognome;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCognome() {
+        return cognome;
+    }
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+}
